49cb60c0a6b9b164a8fd1d378e6e3ea8fc8b3052
cj44444 <53230717+cj44444@users.noreply.github.com>
2019-07-27 15:16:27 +0530

    Update dectobin.py

--- dectobin.py
+++ dectobin.py
@@ -1,4 +1,13 @@
-dec = input()
-if dec != 'x':
-    decimal = int(dec)
-    print(bin(decimal))
+numb = int(input())
+binary = '' 
+
+while(numb):
+ if(numb % 2 == 0):
+  binary += '0'
+ else:
+  binary += '1'
+ numb = numb//2
+
+''.join(reversed(binary))
+
+print(binary)
